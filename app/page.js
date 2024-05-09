c62b1806a4fbfb06775ca6ca3ea3a8d01b086e45
Lansarbac2020 <lansarbacoro@gmail.com>
2024-05-09 19:35:21 +0300

    adding darkmode and update footer

--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
 "use client"
 import { UserButton, useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "next-themes";
+import { MoonIcon, SunIcon } from "lucide-react";
 
 export default function Home() {
   const router = useRouter();
    const {user, isLoaded} = useUser();
+   const [isDarkMode, setIsDarkMode] = useState(false);
+
+   const toggleTheme=()=>{
+    setIsDarkMode(!isDarkMode);
+    document.documentElement.classList.toggle('dark');
+  };
 
   useEffect(()=>{
  if(user)
@@ -19,9 +27,23 @@ export default function Home() {
 
   },[user])
   return (
-   <div>
+ 
+        <main className="container"> 
+        <div className="content">
     <UserButton afterSignOutUrl="/sign-in" />
+<button aria-label="Toggle Dark Mode" className="toggle-button"onClick={toggleTheme}
+>
+  {isDarkMode ? (
+    <MoonIcon className="icon" size={20}/>
+  ):(
+    <SunIcon className="icon" size={20}/>
+  )
+}
 
-   </div>
+</button>
+</div>
+   </main>
+
+ 
   );
 }
